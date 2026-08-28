d3df07c74ec55c48aa83440bd3274a9fc417d889
Rifqy Muhamad Ibrahim <rifqymibra09@gmail.com>
2026-08-28 22:37:33 +0700

    add script for player and environtment

---
+++ player/script/player.gd
@@ -0,0 +1,21 @@
+class_name player extends CharacterBody2D
+
+
+func _process(delta: float) -> void:
+	
+	pass
+
+
+
+
+func _physics_process(delta: float) -> void:
+	velocity.x = 0
+
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -100
+	elif Input.is_action_pressed("move_right"):
+		velocity.x = 100
+
+	velocity.y = velocity.y + 980 * delta
+	move_and_slide()
+	pass
